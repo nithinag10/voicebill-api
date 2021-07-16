cf916232c670142dd2316ba3389201ad7495b971
nithinag <nithinag10@gmail.com>
2021-07-16 21:33:39 +0530

    trying from morning

--- audio_processing/routes.py
+++ audio_processing/routes.py
@@ -3,14 +3,12 @@ from flask import request , jsonify
 from app import app
 
 
-@app.route('/additem')
+@app.route('/additem' , methods=['POST'])
 def process_input():
-
-    return 'do something'
-    # processor = Processor()
-    # data = request.get_json(force=True)
-    # path = processor.decode_base64_string(data['string'])
-    # text = processor.covert_speech_to_text(path)
-    # return text
+    processor = Processor()
+    data = request.get_json(force=True)
+    path = processor.decode_base64_string(data['string'])
+    text = processor.covert_speech_to_text(path)
+    return text
 
 
