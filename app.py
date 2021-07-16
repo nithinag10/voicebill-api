c5b5c0716f43df40e05d7a803a0263bce5603d92
nithinag <nithinag10@gmail.com>
2021-07-16 18:34:50 +0530

    some changed

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from pymongo import MongoClient
 from datetime import datetime
 app = Flask(__name__)
+
+
+client = MongoClient("mongodb+srv://RFIDpayments:Ff6RfZyRN5arkgvz@payments-ukurt.mongodb.net/test?retryWrites=true&w=majority")
+db = client["voice_bill"]
+collection = db["items"]
 
 @app.route('/')
 def homepage():
