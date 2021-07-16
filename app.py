f5103bab82736dc874c86002bac16400da89ab67
nithinag <nithinag10@gmail.com>
2021-07-16 21:25:01 +0530

    lasttry

--- app.py
+++ app.py
@@ -8,12 +8,12 @@ client = MongoClient("mongodb+srv://RFIDpayments:Ff6RfZyRN5arkgvz@payments-ukurt
 db = client["voice_bill"]
 collection = db["items"]
 
+
 @app.route('/')
 def homepage():
     return "hello macha its working"
 
-
-from audio_processing import routes
+from audio_processing.routes import *
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+    app.run(use_reloader=True)
