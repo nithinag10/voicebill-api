66e2a0c85752ecb8e160ebaa2f0251e0e0958b09
nithinag <nithinag10@gmail.com>
2021-07-16 21:28:03 +0530

    still owrking

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ collection = db["items"]
 def homepage():
     return "hello macha its working"
 
-from audio_processing.routes import *
+from audio_processing.routes import process_input
 
 if __name__ == '__main__':
-    app.run(use_reloader=True)
+    app.run(debug=True, use_reloader=True)
